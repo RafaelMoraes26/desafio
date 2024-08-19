e30973675a000e5548cefd524b1e8f2e4c2c872e
mores_rafael <moraes_rafael@terceiros.sicredi.com.br>
2024-08-18 22:06:01 -0300

    feat: add new fields in entity Communication

--- src/main/java/com/luizalabs/desafio/repository/entity/Communication.java
+++ src/main/java/com/luizalabs/desafio/repository/entity/Communication.java
@@ -1,10 +1,20 @@
 package com.luizalabs.desafio.repository.entity;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.luizalabs.desafio.enums.CommunicationSendStatus;
+import com.luizalabs.desafio.enums.CommunicationType;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor
 public class Communication {
 
     @Id
@@ -13,8 +23,38 @@ public class Communication {
 
     @Column(nullable = false)
     private String message;
+
     @Column(nullable = false)
     private String receiver;
+
     @Column(name = "scheduled_time", nullable = false)
-    private LocalDateTime scheduledTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
+    private OffsetDateTime scheduledTime;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CommunicationType type;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CommunicationSendStatus status;
+
+    @JsonIgnore
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @JsonIgnore
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = OffsetDateTime.now();
+        updatedAt = OffsetDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = OffsetDateTime.now();
+    }
 }
