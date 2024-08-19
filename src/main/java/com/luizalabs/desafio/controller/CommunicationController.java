cdd9857b070de1807672a65e46d96331701e36de
mores_rafael <moraes_rafael@terceiros.sicredi.com.br>
2024-08-18 22:08:40 -0300

    feat: changes in controller

--- src/main/java/com/luizalabs/desafio/controller/CommunicationController.java
+++ src/main/java/com/luizalabs/desafio/controller/CommunicationController.java
@@ -1,8 +1,13 @@
 package com.luizalabs.desafio.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import com.luizalabs.desafio.dto.request.CommunicationRequest;
+import com.luizalabs.desafio.dto.response.CommunicationResponse;
+import com.luizalabs.desafio.exception.CommunicationNotFoundException;
+import com.luizalabs.desafio.service.CommunicationService;
+
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -10,4 +15,34 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 class CommunicationController {
 
+    private final CommunicationService service;
+
+
+    CommunicationController(CommunicationService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/")
+    public CommunicationResponse createCommunication(@Valid @RequestBody CommunicationRequest request) {
+        log.info(">>> createCommunication");
+        return service.create(request);
+    }
+
+    @GetMapping("/scheduleStatus/{id}")
+    public CommunicationResponse getScheduleStatus(@PathVariable Long id) throws CommunicationNotFoundException {
+        log.info(">>> getScheduleStatus");
+        return service.getScheduleStatusById(id);
+    }
+
+    @GetMapping("/cancelSend/{id}")
+    public CommunicationResponse cancelSend(@PathVariable Long id) throws CommunicationNotFoundException {
+        log.info(">>> cancelSend");
+        return service.cancelSendById(id);
+    }
+
+    @GetMapping("/sendToQueue/{id}")
+    public void sendCommunication(@PathVariable Long id) throws CommunicationNotFoundException {
+        log.info(">>> sendCommunication");
+        service.sendCommunication(id);
+    }
 }
